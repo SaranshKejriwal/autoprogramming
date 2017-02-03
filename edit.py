93acb753401a9f859f4824ad42704a6de782b997
Saransh Kejriwal <saransh94@gmail.com>
2017-02-04 00:37:08 +0530

    main code that calls the code editor

---
+++ edit.py
@@ -0,0 +1,25 @@
+import fileinput
+from random import randint #for random threshold of filter 
+from functions import *
+with open("edit.py", "r+") as f:
+	#olddata = f.read() # read everything in the file
+	f.seek(0) # rewind
+	print "11"
+	editMe()
+	#f.close()
+	#newdata = olddata.replace("filter_func()","filter_func2()")
+	#filter_func2()
+	#f = open(fileout,'w')
+	#f.write(newdata)
+	#f.close()
+
+'''
+
+f.write("new line\n" + old) # write the new line before
+f = open(filein,'r')
+filedata = f.read()'''
+
+
+
+
+
