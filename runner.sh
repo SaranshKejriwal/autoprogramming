de5fc92940cf5d98ceddd8dbbb16df6add2bab41
Saransh Kejriwal <saransh94@gmail.com>
2017-02-04 00:37:52 +0530

    shell to restart edit.py

---
+++ runner.sh
@@ -0,0 +1,7 @@
+#this shell script will rerun the main code after it has been edited
+echo "in runner"
+#sleep 20
+pkill -f edit.py #first stops the running old script
+sleep 1 #wait for 5 seconds before restart
+python edit.py
+
