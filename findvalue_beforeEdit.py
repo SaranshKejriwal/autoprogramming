05403427c2fa0fb378819e06e9cc060afc4acf61
Saransh Kejriwal <saransh94@gmail.com>
2017-02-04 10:52:24 +0530

    findvalue.py before autoprog

---
+++ findvalue_beforeEdit.py
@@ -0,0 +1,21 @@
+import fileinput
+from random import randint #for random threshold of filter 
+from functions import *
+
+'''This script uses autowriting to modify the function calls and avoid the loop needed to find the optimum value'''
+opt=26
+guess=0
+guess=findOptimum(opt)
+
+if(guess==opt):
+	print "Guess is optimum"
+
+
+
+
+
+
+
+
+
+
